83f358dceb679c4d907ed0c33e981633630744e0
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-06-18 19:19:09 +0530

    Update week4a.ino

--- iot lab/week4a.ino
+++ iot lab/week4a.ino
@@ -1,20 +1,16 @@
 #include<Stepper.h>
-const float STEPS_PER_REV = 32;
-const float GEAR_RED = 64;
-const float STEPS_PER_OUT_REV = STEPS_PER_OUT_REV*GEAR_RED;
-int stepsRequired
-Stepper steppermotor(STEPS_PER_REV,2,4,3,5);
+const float STEPS_PER_REV = 200;
+Stepper mystepper(STEPS_PER_REV,2,4,3,5);
 void setup() {
+  mystepper.setSpeed(60);
   Serial.begin(9600);
 }
 
 void loop() {
-  steppermotor.setSpeed(1);
-  stepsRequired = 4;
-  steppermotor.steps(stepsRequired);
-  delay(200);
-  stepsRequired = STEPS_PER_OUT_REV;
-  Serial.println(stepsRequired);
-  delay(1000);
-  stepsRequired =
+  Serial.println("clo9ckwise");
+  mystepper.step(STEPS_PER_REV);
+  delay(500);
+  Serial.println("Counter Clockwise");
+  mystepper.step(STEPS_PER_REV);
+  delay(500);
 }
