4189d235ac0b5fee198ded5996fb43bf647c9f93
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-06-18 19:16:45 +0530

    Update week3.ino

--- iot lab/week3.ino
+++ iot lab/week3.ino
@@ -1,9 +1,7 @@
-// C++ code
-//
-int RED = 5,BLUE = 4,GREEN = 3;
+
+int RED = 9,BLUE = 10,GREEN = 11;
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);  
   pinMode(BLUE, OUTPUT);
@@ -11,28 +9,16 @@ void setup()
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);  
+  RGB(255,0,0);
   delay(1000);
-  digitalWrite(RED, HIGH);
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(BLUE, LOW);
+  RGB(0,255,0);
   delay(1000);
-  digitalWrite(RED, LOW);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, HIGH);
+  RGB(0,0,255);
   delay(1000);
-  analogWrite(RED,100);  
-  delay(1000);
-  analogWrite(GREEN,100);  
-  delay(1000);
-  analogWrite(RED,255); 
-  delay(1000);
-  analogWrite(BLUE,100);  
+}
 
+void RGB(int R,int G,int B){
+  analogWrite(9,R);
+  analogWrite(10,G);
+  analogWrite(11,B);
 }
