e8e74dc526d1a45e08362379f9b1d1e7266e0474
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-06-20 05:08:11 +0530

    update

--- iot lab/week2.ino
+++ iot lab/week2.ino
@@ -21,3 +21,17 @@ void loop()
   noTone(8);
   delay(500);
 }
+
+
+// void setup(){
+// pinMode(4,OUTPUT);
+// pinMode(6,OUTPUT);
+// }
+// void loop(){
+// digitalWrite(4,HIGH);
+// digitalWrite(6,HIGH);
+// delay(1000);
+// digitalWrite(4,LOW);
+// digitalWrite(6,LOW);
+// delay(1000);
+// }
