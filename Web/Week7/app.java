7f185414b96aab2ecc308835eb1414a249c83665
Mrdecent08 <60995685+Mrdecent08@users.noreply.github.com>
2022-06-28 11:49:40 +0530

    Update app.java

--- Web/Week7/app.java
+++ Web/Week7/app.java
@@ -31,3 +31,8 @@ public class app extends HttpServlet{
         }
     }
 }
+
+
+
+// Class.forName("oracle.jdbc.driver.OracleDriver");
+// Connection con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/" + dbName, userName,password);
