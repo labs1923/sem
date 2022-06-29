f088b25d8023cd4a603efc73038f4d96eb8a516c
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-06-29 07:12:40 +0530

    Update

--- R Programs/R Statistics/Simple_BarPlot.R
+++ R Programs/R Statistics/Simple_BarPlot.R
@@ -2,4 +2,4 @@
 x = c(31,42,53,64,75)
 labels = c("Eng", "Math", "Sci", "Soc", "Chem")
 colors = c("red","green","blue","orange","violet")
-barplot(x, name.arg=labels, main="Subject Marks", col=colors)
+barplot(x, names.arg=labels, main="Subject Marks", col=colors)
