5f10010ceb33b20d82230222e31783c41298df77
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-06-29 08:28:25 +0530

    Update

--- R Programs/R Statistics/PieChart_Pink_60_Degrees.R
+++ R Programs/R Statistics/PieChart_Pink_60_Degrees.R
@@ -1,4 +1,5 @@
 # R program to create a simple pie chart of three subject's marks, change the border color to pink and make inside bar lines as 60 degrees.
 x = c(50,60,70)
-labels = c("Eng", "Math", "Sci")
-pie(x, labels=labels, main="Subject Marks", border="pink", density=10, angle=60)
+label = c("Eng", "Math", "Sci")
+colors = c("green","red","blue")
+pie(x, labels=label, col=colors, main="Subject Marks", border="pink", density=10, angle=60)
